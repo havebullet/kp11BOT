6e75ea5b2b8b8e3981da8e28654b3371b5a6cc01
Anastasia Biryulkina <63350367+havebullet@users.noreply.github.com>
2020-12-13 19:51:12 +0300

    Lesson 4 opros
    Работает немного по пожилому, не могу разобраться с кодом

---
+++ opros4lesson/config.py
@@ -0,0 +1,8 @@
+API_TOKEN = '1451124657:AAFxOcvLw7kdMmFSmfoNabYmd7iXs_Oppms'
+admin_id = 1190397076
+
+# Database connection setting
+dbHost = 'localhost'
+dbUser = 'root'
+dbPassword = '12345'
+dbName =  'telegrum'
