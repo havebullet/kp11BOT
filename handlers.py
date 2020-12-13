79b3926ef607948b5c2c18bca0dac5c2d2769e39
Anastasia Biryulkina <63350367+havebullet@users.noreply.github.com>
2020-12-13 22:55:32 +0300

    Delete handlers.py

--- handlers.py
+++
@@ -1,8 +0,0 @@
-# Import library
-from main import bot, dp
-from aiogram import types
-from aiogram.types import Message
-
-# Send message to admin
-async def send_to_admin(dp):
-	await bot.send_message(chat_id=secret, text="Hello")
